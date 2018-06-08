510b6e45bb2d1ad107b3a81a3ba4cd52411e1d34
EugenyaS <39557015+EugenyaS@users.noreply.github.com>
2018-06-08 09:52:34 +0300

    Update task2.js
    убрала функцию, учла случай - строка 1=строка 2, убрала лишнее условие после ввода fl

--- task2.js
+++ task2.js
@@ -1,21 +1,25 @@
 /* Task 2 */
-function TestF(input1, input2) {
-    if (input1.length==input2.length)
-	{        
-        for (var i=0; i<input1.length;i++)
-        {
-          input2=input2.concat(input2[0]).substring(1);
-          if (~input1.indexOf(input2))
-		  { 
-			return i+1;
-		  }
-        }
-        return -1;
-	}
+var input1=process.argv[2];
+var input2=process.argv[3];
+var fl=-1;
+if (input1.length==input2.length)
+{ 
+    if (~input1.indexOf(input2)) 
+    {
+       fl=0;
+    } 
     else
-    {  
-	    return -1; 
-	}
-}
-
-process.stdout.write(TestF(process.argv[2],process.argv[3]);
+    {
+	for (var i=0; i<input1.length;i++)
+        {
+            input2=input2.concat(input2[0]).substring(1);
+            if (~input1.indexOf(input2))
+            { 
+		fl=i+1;
+                break;
+	    }          
+         }          
+     }
+};  
+  
+process.stdout.write(fl);
