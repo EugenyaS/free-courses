657531deb5f695e721093d625c55d2cff096aab9
EugenyaS <39557015+EugenyaS@users.noreply.github.com>
2018-06-09 14:22:33 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -2,24 +2,18 @@
 var input1=process.argv[2];
 var input2=process.argv[3];
 var fl=-1;
-if (input1.length==input2.length)
-{ 
-    if (~input1.indexOf(input2)) 
-    {
+if (input1.length==input2.length) { 
+    if (~input1.indexOf(input2)) {
        fl=0;
     } 
-    else
-    {
-	for (var i=0; i<input1.length;i++)
-        {
+    else  {
+	for (var i=0; i<input1.length;i++) {
             input2=input2.concat(input2[0]).substring(1);
-            if (~input1.indexOf(input2))
-            { 
+            if (~input1.indexOf(input2)) { 
 		fl=i+1;
                 break;
 	    }          
          }          
      }
-};  
-  
+};
 process.stdout.write(String(fl));
