0ed9ef91208d4fc0e9463a9fc9ecfd5a4f057e82
EugenyaS <39557015+EugenyaS@users.noreply.github.com>
2018-06-10 07:12:31 +0300

    сдвиг в другую сторону
    Проверка сдвига в обратную сторону

--- task2.js
+++ task2.js
@@ -7,13 +7,24 @@ if (input1.length==input2.length) {
        fl=0;
     } 
     else  {
-	for (var i=0; i<input1.length;i++) {
+      var fl1=-1;
+	  for (var i=0; i<=input1.length;i++) {
             input2=input2.concat(input2[0]).substring(1);
-            if (~input1.indexOf(input2)) { 
-		fl=i+1;
+            if (~input1.indexOf(input2)) {              
+	        	fl1=i+1;
                 break;
-	    }          
-         }          
-     }
+	        }          
+      }     
+      input2=process.argv[3];
+      var fl2=-1;
+      for (var i=1; i<input1.length;i++) {
+            input2=input2.substring(input1.length-1).concat(input2.substring(0,input1.length-1));
+            if (~input1.indexOf(input2)) {       
+		       fl2=i;
+               break;
+	        }                          
+      }  
+      if(fl1<fl2) {fl=fl1} else (fl=fl2)
+   }
 };
 process.stdout.write(String(fl));
